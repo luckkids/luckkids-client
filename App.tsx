afa0573fc433276167a7a1804320a35a81c072a1
gil <kyg4006@gmail.com>
2023-10-08 17:15:10 +0900

    [add] 스택 네이게이션 패키지추가

--- App.tsx
+++ App.tsx
@@ -5,28 +5,20 @@
  * @format
  */
 
-import { QueryClientProvider } from '@queries';
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native';
-import { RecoilRoot } from 'recoil';
-import { Layout as L } from '@design-system';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FrameLayout from './src/frame/frame.layout';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <RecoilRoot>
-      {/* <ThemeProvider theme={Colors}> */}
-      <QueryClientProvider>
-        <SafeAreaView>
-          <StatusBar />
-          <ScrollView contentInsetAdjustmentBehavior="automatic">
-            <L.FlexRow>
-              <Text>Luck Maker</Text>
-            </L.FlexRow>
-          </ScrollView>
-        </SafeAreaView>
-      </QueryClientProvider>
-      {/* </ThemeProvider> */}
-    </RecoilRoot>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={FrameLayout} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/frame/frame.layout.tsx
@@ -0,0 +1,26 @@
+import React, { JSX } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native';
+import { Layout as L } from '@design-system';
+import { QueryClientProvider } from '@queries';
+import { RecoilRoot } from 'recoil';
+
+function FrameLayout(): JSX.Element {
+  return (
+    <RecoilRoot>
+      {/* <ThemeProvider theme={Colors}> */}
+      <QueryClientProvider>
+        <SafeAreaView>
+          <StatusBar />
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <L.FlexRow>
+              <Text>Luck Maker</Text>
+            </L.FlexRow>
+          </ScrollView>
+        </SafeAreaView>
+      </QueryClientProvider>
+      {/* </ThemeProvider> */}
+    </RecoilRoot>
+  );
+}
+
+export default FrameLayout;
