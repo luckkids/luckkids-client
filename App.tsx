fb5c4257995b60fd9d89f9396c3e0a8bcd9a4be1
gil <kyg4006@gmail.com>
2023-10-10 00:06:34 +0900

    [delete] 불필요 코드 삭제

--- App.tsx
+++ App.tsx
@@ -8,13 +8,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { QueryClientProvider } from '@queries';
 import { RecoilRoot } from 'recoil';
+import { QueryClientProvider } from '@queries';
 import { RouteTabNavigator } from './src/route/route.tab.navigator';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
 function App(): JSX.Element {
   return (
--- src/frame/frame.layout.tsx
+++ src/frame/frame.layout.tsx
@@ -1,7 +1,11 @@
-import React, { JSX } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
 import { Layout as L } from '@design-system';
 
+/**
+ * 임시로 any 타입 지정
+ * todo: 세팅 후 타입 생성하기
+ * */
 export const FrameLayout = (props: any) => {
   return (
     <SafeAreaView>
