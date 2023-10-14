78a467e1028b6a89a0018bb3ddc5662c9a46c1ec
gil <kyg4006@gmail.com>
2023-10-14 14:30:26 +0900

    [modify] merge : origin

--- src/frame/frame.layout.tsx
+++ src/frame/frame.layout.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
-import { Layout as L } from '@design-system';
 
 /**
  * 임시로 any 타입 지정
  * todo: 세팅 후 타입 생성하기
  * */
+
 export const FrameLayout = (props: any) => {
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <L.FlexRow>{props.children}</L.FlexRow>
+        {props.children}
       </ScrollView>
     </SafeAreaView>
   );
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback } from 'react-native';
 import { FrameLayout } from '../frame/frame.layout';
+import { L } from '@design-system';
+import AlertPopup from '@global-components/common/AlertPopup/AlertPopup';
+import BottomSheet from '@global-components/common/BottomSheet/BottomSheet';
+import LoadingIndicator from '@global-components/common/LoadingIndicator/LoadingIndicator';
+
+const handlePressBottomSheet = () => {
+  BottomSheet.show({
+    component: <Text>내용</Text>,
+  });
+};
+
+const handlePressAlertPopup = () => {
+  AlertPopup.show({
+    title: '제목',
+    body: '내용',
+    noText: '취소',
+    yesText: '확인',
+  });
+};
+
+const handlePressLoadingIndicator = () => {
+  LoadingIndicator.show({});
+  setTimeout(() => {
+    LoadingIndicator.hide();
+  }, 3000);
+};
 
 export const Home = () => {
   return (
     <FrameLayout>
-      <Text>Home</Text>
+      <L.Row p={24}>
+        <Text>Luck Maker</Text>
+      </L.Row>
+      <L.Row>
+        <TouchableWithoutFeedback onPress={handlePressBottomSheet}>
+          <Text>Bottom Sheet</Text>
+        </TouchableWithoutFeedback>
+      </L.Row>
+      <L.Row>
+        <TouchableWithoutFeedback onPress={handlePressAlertPopup}>
+          <Text>Alert Popup</Text>
+        </TouchableWithoutFeedback>
+      </L.Row>
+      <L.Row>
+        <TouchableWithoutFeedback onPress={handlePressLoadingIndicator}>
+          <Text>Loading Indicator</Text>
+        </TouchableWithoutFeedback>
+      </L.Row>
     </FrameLayout>
   );
 };
