1d0137523217eedeff7576e3496af22bb4047852
Gina <koyrkr@gmail.com>
2023-10-28 17:47:05 -0700

    gina/refactor/screen-stack (#4)
    * [add] 테스트 코드 추가
    
    * [fix] conflict 제거

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, TouchableWithoutFeedback } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { Button, Font, L } from '@design-system';
 import { FrameLayout } from '../frame/frame.layout';
 import AlertPopup from '@global-components/common/AlertPopup/AlertPopup';
@@ -31,6 +32,15 @@ export const Home = () => {
       LoadingIndicator.hide();
     }, 1000);
   };
+
+  const navigation = useNavigation<
+    NavigationProp<{
+      // TODO(gil): 타입 추론 가능하도록 수정 필요?
+      마이: undefined;
+      Test: undefined;
+      HomeTest: undefined;
+    }>
+  >();
   return (
     <FrameLayout>
       <L.Col p={24}>
@@ -87,6 +97,34 @@ export const Home = () => {
       <L.Row>
         <TouchableWithoutFeedback onPress={handlePressLoadingIndicator}>
           <Text>Loading Indicator</Text>
+        </TouchableWithoutFeedback>
+      </L.Row>
+
+      <L.Row mt={40}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            navigation.navigate('마이');
+          }}
+        >
+          <Text>마이탭으로 이동</Text>
+        </TouchableWithoutFeedback>
+      </L.Row>
+      <L.Row mt={40}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            navigation.navigate('Test');
+          }}
+        >
+          <Text>Test</Text>
+        </TouchableWithoutFeedback>
+      </L.Row>
+      <L.Row mt={40}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            navigation.navigate('HomeTest');
+          }}
+        >
+          <Text>HomeTest</Text>
         </TouchableWithoutFeedback>
       </L.Row>
     </FrameLayout>
