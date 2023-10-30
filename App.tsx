d986c2761f7be3ff9e318b673a909719f3cd1a8d
koyrkr <koyrkr@gmail.com>
2023-10-29 17:58:56 -0700

    [fix] 글로벌 컴포넌트 관련 에러 수정

--- App.tsx
+++ App.tsx
@@ -6,69 +6,50 @@
  */
 
 import React from 'react';
-// import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { IPage } from '@types-common/page.types';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components/native';
 import { Colors } from '@design-system';
 import { QueryClientProvider } from '@queries';
 import { DataStackScreen } from './src/data/data.stack.screen';
-import { IPage } from '@types-common/page.types';
-
-/*import { BottomTabNavigator } from './src/route/route.tab.navigator';
-import withGlobalComponents from '@hooks/hoc/withGlobalComponents';*/
+import withGlobalComponents from '@hooks/hoc/withGlobalComponents';
 
 const App: React.FC<IPage> = () => {
-  const Stack = createNativeStackNavigator();
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={Colors}>
-        <QueryClientProvider>
-          <NavigationContainer>
-            <Stack.Navigator>
-              {DataStackScreen.map((item, i) => {
-                return (
-                  <Stack.Screen
-                    name={item.name}
-                    component={item.component}
-                    options={
-                      item.options ? item.options : { headerShown: false }
-                    }
-                    key={i}
-                  />
-                );
-              })}
-            </Stack.Navigator>
-          </NavigationContainer>
-        </QueryClientProvider>
-      </ThemeProvider>
-    </RecoilRoot>
+    <GestureHandlerRootView style={{ flexGrow: 1 }}>
+      <RecoilRoot>
+        <ThemeProvider theme={Colors}>
+          <QueryClientProvider>
+            <RootNavigator />
+          </QueryClientProvider>
+        </ThemeProvider>
+      </RecoilRoot>
+    </GestureHandlerRootView>
   );
 };
 
-/*const RootNavigator = withGlobalComponents(() => {
-    const TestComponent = () => {
-        return <Text>without bottomtab</Text>;
-    };
+const Stack = createNativeStackNavigator();
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="BottomTabNavigator"
-                    component={BottomTabNavigator}
-                    options={{ headerShown: false }}
-                />
-                {/!* 바텀탭 필요없는 스크린들 *!/}
-                <Stack.Screen
-                    name="Test"
-                    component={TestComponent}
-                    options={{ headerShown: false }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-});*/
+const RootNavigator = withGlobalComponents(() => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {DataStackScreen.map((item, i) => {
+          return (
+            <Stack.Screen
+              name={item.name}
+              component={item.component}
+              options={item.options ? item.options : { headerShown: false }}
+              key={i}
+            />
+          );
+        })}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+});
 
 export default App;
