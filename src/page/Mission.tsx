166afaa5a939a16766678f92a1de761cba31e8a5
gil <kyg4006@gmail.com>
2023-11-08 23:21:26 +0900

    [modify] 미션 페이지 ui 작업

--- src/page/Mission.tsx
+++ src/page/Mission.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Button } from '@design-system';
+import { Text, View } from 'react-native';
+import { Button, Font, L } from '@design-system';
 import { FrameLayout } from '@frame/frame.layout';
 import { AppScreens, IPage } from '@types-common/page.types';
+import FloatingButton from '@components/common/FloatingButton/FloatingButton';
 
 export const Mission: React.FC<IPage> = (props) => {
   return (
-    <FrameLayout>
-      <Text>Mission</Text>
-      <Button
-        type={'action'}
+    <>
+      <FrameLayout>
+        <L.Row>
+          <Font type={'FOOTNOTE_REGULAR'}>클로버</Font>
+          <Font type={'FOOTNOTE_REGULAR'}>+3</Font>
+        </L.Row>
+        <L.Row p={24} justify={'space-between'}>
+          <Font type={'LARGE_TITLE_BOLD'}>미션 달성하기</Font>
+          <Font type={'LARGE_TITLE_REGULAR'} color={'LUCK_GREEN'}>
+            3/10
+          </Font>
+        </L.Row>
+      </FrameLayout>
+      <FloatingButton
         text={'습관편집'}
         onPress={() => props.navigation.navigate(AppScreens.MissionRepair)}
-        sizing="stretch"
-        bgColor={'LUCK_GREEN'}
       />
-    </FrameLayout>
+    </>
   );
 };
