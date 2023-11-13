0a8b582620e598c3e1b4e7eb9e3d058a352fb68b
gil <kyg4006@gmail.com>
2023-11-13 23:30:59 +0900

    [fix] fetch 추가

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -39,29 +39,34 @@ export const useFetch = (args: {
       }
     };
 
-    loadData().then((result) => {
-      if (result.statusCode === STATUS.UNAUTHORIZED) {
-        //1. 토큰 만료시 리프레시 토큰으로 엑세스 토큰 재발행
-        return setExpiredAccessToken();
-      }
-      if (result.data.refreshToken) {
-        //2. 토큰이 있으면 recoil.token.ts에 저장(글로벌로 참조 가능하도록)
-        setToken({
-          accessToken: result.data.accessToken,
-          refreshToken: result.data.refreshToken,
-        });
-      }
-      if (result.statusCode === STATUS.SUCCESS) {
-        //3. 결과가 200일경우 isSuccess 불리언값 참조 가능
-        setIsSuccess(true);
-        //4. 완료 후 컴퍼넌트에서 던져주는 결과후 실행할 함수 실행
-        args.onSuccessCallback && args.onSuccessCallback();
-      }
-      console.log('RESULT(data) ====>', result.data);
-      //5. 데이터 참조 가능하도록 추가
-      setResultData(result.data);
-      return result.data;
-    });
+    loadData()
+      .then((result) => {
+        if (result.statusCode === STATUS.UNAUTHORIZED) {
+          //1. 토큰 만료시 리프레시 토큰으로 엑세스 토큰 재발행
+          return setExpiredAccessToken();
+        }
+        if (result.data.refreshToken) {
+          //2. 토큰이 있으면 recoil.token.ts에 저장(글로벌로 참조 가능하도록)
+          setToken({
+            accessToken: result.data.accessToken,
+            refreshToken: result.data.refreshToken,
+          });
+        }
+        if (result.statusCode === STATUS.SUCCESS) {
+          //3. 결과가 200일경우 isSuccess 불리언값 참조 가능
+          setIsSuccess(true);
+          //4. 완료 후 컴퍼넌트에서 던져주는 결과후 실행할 함수 실행
+          args.onSuccessCallback && args.onSuccessCallback();
+        }
+        console.log('RESULT(data) ====>', result.data);
+        //5. 데이터 참조 가능하도록 추가
+        setResultData(result.data);
+        return result.data;
+      })
+      .catch(() => {
+        setIsSuccess(false);
+        console.log('서버 통신 에러');
+      });
   }, []);
 
   const setExpiredAccessToken = async () => {
