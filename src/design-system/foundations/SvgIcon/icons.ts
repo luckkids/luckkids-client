b2cf1f96e52ffe494d9b581764e5e2b626d12ae2
gil <kyg4006@gmail.com>
2023-11-18 19:14:06 +0900

    [add] icon 추가(arrow_left, lucky_uncheck)

--- src/design-system/foundations/SvgIcon/icons.ts
+++ src/design-system/foundations/SvgIcon/icons.ts
@@ -1,15 +1,19 @@
 import bell from '../../assets/icons/bell.svg';
 import bell_badge from '../../assets/icons/bell_badge.svg';
 import lucky_check from '../../assets/icons/lucky_check.svg';
+import lucky_uncheck from '../../assets/icons/lucky_uncheck.svg';
 import password_visibility_off from '../../assets/icons/password_visibility_off.svg';
 import password_visibility_on from '../../assets/icons/password_visibility_on.svg';
 import validation_check from '../../assets/icons/validation_check.svg';
+import arrow_left from '../../assets/icons/arrow_left.svg';
 
 export {
   bell,
   bell_badge,
   lucky_check,
+  lucky_uncheck,
   validation_check,
   password_visibility_off,
   password_visibility_on,
+  arrow_left,
 };
