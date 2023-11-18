cfea30f09e622c71804f973351214048216fe64a
gil <kyg4006@gmail.com>
2023-11-18 21:35:52 +0900

    [add] 아이콘(svg) 추가

--- src/design-system/foundations/SvgIcon/icons.ts
+++ src/design-system/foundations/SvgIcon/icons.ts
@@ -1,19 +1,25 @@
+import album from '../../assets/icons/album.svg';
+import arrow_left from '../../assets/icons/arrow_left.svg';
 import bell from '../../assets/icons/bell.svg';
 import bell_badge from '../../assets/icons/bell_badge.svg';
+import bell_off from '../../assets/icons/bell_off.svg';
+import list from '../../assets/icons/list.svg';
 import lucky_check from '../../assets/icons/lucky_check.svg';
 import lucky_uncheck from '../../assets/icons/lucky_uncheck.svg';
 import password_visibility_off from '../../assets/icons/password_visibility_off.svg';
 import password_visibility_on from '../../assets/icons/password_visibility_on.svg';
 import validation_check from '../../assets/icons/validation_check.svg';
-import arrow_left from '../../assets/icons/arrow_left.svg';
 
 export {
   bell,
   bell_badge,
+  bell_off,
   lucky_check,
   lucky_uncheck,
   validation_check,
   password_visibility_off,
   password_visibility_on,
   arrow_left,
+  album,
+  list,
 };
