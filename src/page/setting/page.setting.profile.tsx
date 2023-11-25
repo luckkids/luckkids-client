ea26466368708b78e20dab3a91aafcf7b9568b62
gil <kyg4006@gmail.com>
2023-11-25 18:53:39 +0900

    [add] 설정 > 프로필수정 스타일 추가

--- src/page/setting/page.setting.profile.tsx
+++ src/page/setting/page.setting.profile.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import { FrameLayout } from '@frame/frame.layout';
 import { AppScreens, IPage } from '@types-common/page.types';
+import { Font, L, TextInputField } from '@design-system';
+import ButtonText from '../../design-system/components/Button/ButtonText';
 
 const S = {
   Text: styled.Text({
@@ -11,13 +13,34 @@ const S = {
 };
 
 export const PageSettingProfile: React.FC<IPage> = (props) => {
+  const [text, setText] = useState<string>('');
   return (
-    <FrameLayout>
-      <S.Text>프로필수</S.Text>
-      <S.Button
-        title={'저장/취소'}
-        onPress={() => props.navigation.navigate(AppScreens.Setting)}
-      />
+    <FrameLayout
+      NavBar={
+        <L.Row pv={12} ph={16} justify={'space-between'}>
+          <ButtonText
+            onPress={() => console.log('cancel')}
+            text={'취소'}
+            fontType={'HEADLINE_SEMIBOLD'}
+            textColor={'LUCK_GREEN'}
+          />
+          <Font type={'HEADLINE_SEMIBOLD'}>프로필 수정</Font>
+          <ButtonText
+            onPress={() => console.log('cancel')}
+            fontType={'HEADLINE_SEMIBOLD'}
+            text={'저장'}
+            textColor={'LUCK_GREEN'}
+          />
+        </L.Row>
+      }
+    >
+      <L.Row ph={25} mt={30}>
+        <TextInputField
+          text={text}
+          onChangeText={setText}
+          placeholder="행운 문구 변경"
+        />
+      </L.Row>
     </FrameLayout>
   );
 };
