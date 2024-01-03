9672420de3a7000993fdf68d6a0d65a827345f6f
gil <longlongroad@naver.com>
2024-01-04 00:59:15 +0900

    [modify] 탈퇴 ui 작업 state 추가

--- src/page/setting/page.setting.account.tsx
+++ src/page/setting/page.setting.account.tsx
@@ -1,21 +1,21 @@
-import React, {useState} from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components/native';
 import { FrameLayout } from '@frame/frame.layout';
 import useNavigationService from '@hooks/navigation/useNavigationService';
-import { Button, L } from '@design-system';
+import { Button, Font, L } from '@design-system';
 import { PageSetting } from '@page/setting/page.setting';
 import { DataStackScreen } from '../../data/data.stack.screen';
 import StackNavBar from '@components/common/StackNavBar/StackNavBar';
-import {TouchableWithoutFeedback} from "react-native";
+import { TouchableWithoutFeedback } from 'react-native';
 
 const S = {
-  Text: styled.Text({
-    textAlign: 'center',
+  itemWrap: styled.View({
+    paddingHorizontal: 25,
+    paddingVertical: 20,
   }),
-  Button: styled.Button({}),
 };
 
-const accountList = [
+const accountText = [
   '잘 사용하지 않는 앱이에요',
   '사용하기 어려워요',
   '알림이 너무 많이 와요',
@@ -25,16 +25,43 @@ const accountList = [
 
 export const PageSettingAccount: React.FC = () => {
   const navigation = useNavigationService();
-  const [active,setActive] = useState<Array<boolean>>([false,false,false,false,false]);
+  const tempArray = [0, 0, 0, 0, 0];
+  const [index, setIndex] = useState<number>(0);
+  useEffect(() => {}, [index]);
+
+  const accountListItem = useMemo(() => {
+    tempArray[index] = 1;
+    return accountText.map((item, i) => {
+      if (tempArray[i] === 1) {
+        return (
+          <S.itemWrap key={i}>
+            <Font type={'BODY_REGULAR'} color={'LUCK_GREEN'}>
+              {item}
+            </Font>
+          </S.itemWrap>
+        );
+      } else {
+        return (
+          <TouchableWithoutFeedback
+            key={i}
+            onPress={() => {
+              setIndex(i);
+              console.log(tempArray);
+            }}
+          >
+            <S.itemWrap>
+              <Font type={'BODY_REGULAR'} color={'WHITE'}>
+                {item}
+              </Font>
+            </S.itemWrap>
+          </TouchableWithoutFeedback>
+        );
+      }
+    });
+  }, [tempArray, index]);
   return (
     <FrameLayout NavBar={<StackNavBar title={'탈퇴하기'} useBackButton />}>
-        <L.Col>
-            {accountList.map((item,i)=>{
-                return <TouchableWithoutFeedback onPress={()}>
-
-                </TouchableWithoutFeedback>
-            })}
-        </L.Col>
+      <L.Col>{accountListItem}</L.Col>
       <L.Col g={10} w={'100%'} ph={25}>
         <L.Row w={'100%'}>
           <Button
@@ -42,6 +69,7 @@ export const PageSettingAccount: React.FC = () => {
             text={'제출'}
             bgColor={'LUCK_GREEN'}
             sizing={'stretch'}
+            status={tempArray.includes(1) ? 'normal' : 'disabled'}
             onPress={() => navigation.navigate('Login')}
           />
         </L.Row>
