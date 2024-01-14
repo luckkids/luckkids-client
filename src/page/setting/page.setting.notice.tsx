8c1ab8e4bdd538bbc11d317093f02959010e86e0
gil <longlongroad@naver.com>
2024-01-14 22:51:47 +0900

    [add] 공지사항 api 연결

--- src/page/setting/page.setting.notice.tsx
+++ src/page/setting/page.setting.notice.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import { FrameLayout } from '@frame/frame.layout';
 import useNavigationService from '@hooks/navigation/useNavigationService';
+import StackNavBar from '@components/common/StackNavBar/StackNavBar';
+import { TouchableWithoutFeedback } from 'react-native';
+import { Font, L } from '@design-system';
+import { useFetch } from '@hooks/useFetch';
+import { IResponse } from '../../types/recoil/types.recoil';
+import { ISettingNotice } from '@types-common/page.types';
 
 const S = {
   Text: styled.Text({
@@ -12,13 +18,32 @@ const S = {
 
 export const PageSettingNotice: React.FC = () => {
   const navigation = useNavigationService();
+  const [data, setData] = useState<Array<ISettingNotice>>([]);
+  const { onFetch } = useFetch({
+    method: 'GET',
+    url: '/notices/',
+    value: {},
+    onSuccessCallback: (rtn) => {
+      setData(rtn);
+    },
+  });
+  useEffect(() => {
+    onFetch();
+  }, []);
   return (
-    <FrameLayout>
-      <S.Text>공지사항</S.Text>
-      <S.Button
-        title={'뒤로가기'}
-        onPress={() => navigation.navigate('Setting')}
-      />
+    <FrameLayout NavBar={<StackNavBar title={'공지사항'} useBackButton />}>
+      {data?.map((item, i) => {
+        return (
+          <TouchableWithoutFeedback onPress={() => console.log('aa')} key={i}>
+            <L.Col pv={25} ph={25}>
+              <Font type={'BODY_REGULAR'}>{item.title}</Font>
+              <Font type={'SUBHEADLINE_REGULAR'} color={'GREY1'}>
+                {item.createdDate}
+              </Font>
+            </L.Col>
+          </TouchableWithoutFeedback>
+        );
+      })}
     </FrameLayout>
   );
 };
