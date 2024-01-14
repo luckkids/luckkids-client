0fa7ce7c1ade50f700b092ce6d88e93f02b9d099
koyrkr <koyrkr@gmail.com>
2024-01-14 04:17:34 -0800

    feat: add animation for home screen

--- src/components/page/home/home.week.calendar.tsx
+++ src/components/page/home/home.week.calendar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { format } from 'date-fns';
 import { LocaleConfig } from 'react-native-calendars';
 import { Font, L } from '@design-system';
+import { TouchableWithoutFeedback } from 'react-native';
+import useNavigationService from '@hooks/navigation/useNavigationService';
 
 LocaleConfig.locales.ko = {
   monthNames: [
@@ -47,7 +49,8 @@ LocaleConfig.locales.ko = {
 LocaleConfig.defaultLocale = 'ko';
 
 const HomeWeekCalendar: React.FC = () => {
-  const today = new Date('2024-01-29');
+  const navigation = useNavigationService();
+  const today = new Date();
   const weekDates = [];
   const koreanDays = ['일', '월', '화', '수', '목', '금', '토'];
 
@@ -66,35 +69,47 @@ const HomeWeekCalendar: React.FC = () => {
     });
   }
 
+  const handlePressCalendar = () => {
+    navigation.navigate('HomeCalendar');
+  };
+
   return (
-    <L.Row mt={8} ph={30} w="100%" g={8} justify="center" items="center">
-      {weekDates.map((item) => {
-        const isToday = item.day === format(today, 'd');
-        const isFuture = item.date > format(today, 'yyyy-MM-dd');
-        return (
-          <L.Col g={12.5} key={item.day} flex-1 items="center" justify="center">
-            <L.Row
-              w={23}
-              h={23}
-              bg={isToday ? 'WHITE' : 'TRANSPARENT'}
-              rounded={100}
-              justify="center"
+    <TouchableWithoutFeedback onPress={handlePressCalendar}>
+      <L.Row mt={8} ph={30} w="100%" g={8} justify="center" items="center">
+        {weekDates.map((item) => {
+          const isToday = item.day === format(today, 'd');
+          const isFuture = item.date > format(today, 'yyyy-MM-dd');
+          return (
+            <L.Col
+              g={12.5}
+              key={item.day}
+              flex-1
               items="center"
+              justify="center"
             >
-              <Font
-                type="BODY_SEMIBOLD"
-                color={isToday ? 'HOME_TODAY_TEXT' : 'WHITE'}
+              <L.Row
+                w={23}
+                h={23}
+                bg={isToday ? 'WHITE' : 'TRANSPARENT'}
+                rounded={100}
+                justify="center"
+                items="center"
               >
-                {item.text}
+                <Font
+                  type="BODY_SEMIBOLD"
+                  color={isToday ? 'HOME_TODAY_TEXT' : 'WHITE'}
+                >
+                  {item.text}
+                </Font>
+              </L.Row>
+              <Font type="BODY_SEMIBOLD" color={isFuture ? 'WHITE' : 'BLACK'}>
+                {item.day}
               </Font>
-            </L.Row>
-            <Font type="BODY_SEMIBOLD" color={isFuture ? 'WHITE' : 'BLACK'}>
-              {item.day}
-            </Font>
-          </L.Col>
-        );
-      })}
-    </L.Row>
+            </L.Col>
+          );
+        })}
+      </L.Row>
+    </TouchableWithoutFeedback>
   );
 };
 
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import { TouchableWithoutFeedback } from 'react-native';
-import { Colors, Font } from '@design-system';
+import React, { useRef } from 'react';
+import { Animated, TouchableWithoutFeedback, View } from 'react-native';
+import { SCREEN_WIDTH } from '@gorhom/bottom-sheet';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { DEFAULT_MARGIN } from '@constants';
+import { Font, L } from '@design-system';
 import HomeNavbar from '@components/page/home/home.navbar';
 import HomeWeekCalendar from '@components/page/home/home.week.calendar';
 import { FrameLayout } from '@frame/frame.layout';
 import useNavigationService from '@hooks/navigation/useNavigationService';
+
 const bgImage = require('assets/images/home-bg.png');
 
 export const Home: React.FC = () => {
+  const { top } = useSafeAreaInsets();
   const navigation = useNavigationService();
-
-  const handlePressCalendar = () => {
-    navigation.navigate('HomeCalendar');
-  };
 
   const handleEditComment = () => {
     navigation.navigate('HomeComment');
+  };
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  // 애니메이션을 위한 높이와 위치 계산
+  const animatedHeight = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [
+      SUCCESS_RATE_HEIGHT + GAP + LUCKKIDS_HEIGHT - INITIAL_HEIGHT_REDUCTION,
+      SUCCESS_RATE_HEIGHT + GAP + LUCKKIDS_HEIGHT,
+    ],
+  });
+
+  const animatedY = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [50, -INITIAL_HEIGHT_REDUCTION + 30],
+  });
+
+  const openAnimation = () => {
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const closeAnimation = () => {
+    Animated.timing(animatedValue, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
   };
 
   return (
@@ -27,15 +60,72 @@ export const Home: React.FC = () => {
       >
         {/* 캘린더 */}
         <HomeWeekCalendar />
-        {/* <TouchableWithoutFeedback onPress={handlePressCalendar}>
-          <Font type="BODY_SEMIBOLD">캘린더 이동</Font>
-        </TouchableWithoutFeedback> */}
         {/* 캐릭터 메인 */}
-        {/* <TouchableWithoutFeedback onPress={handleEditComment}>
-          <Font type="BODY_SEMIBOLD">행운 문구 수정</Font>
-        </TouchableWithoutFeedback> */}
+
+        <L.Col justify={'center'} items={'center'} mt={100}>
+          <L.Row
+            w={SCREEN_WIDTH - 2 * CHARACTER_MARGIN}
+            h={SCREEN_WIDTH - 2 * CHARACTER_MARGIN}
+            bg={'LUCK_GREEN'}
+          />
+          <TouchableWithoutFeedback onPress={handleEditComment}>
+            <Font type="BODY_SEMIBOLD">행운 문구 수정</Font>
+          </TouchableWithoutFeedback>
+        </L.Col>
         {/* 정보 */}
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <TouchableWithoutFeedback
+            style={{
+              flex: 1,
+            }}
+            onPress={closeAnimation}
+          >
+            <Animated.View
+              style={{
+                height: animatedHeight,
+                paddingHorizontal: DEFAULT_MARGIN,
+                width: '100%',
+                position: 'absolute',
+                right: 0,
+                left: 0,
+                transform: [{ translateY: animatedY }],
+              }}
+              onTouchStart={openAnimation}
+            >
+              <L.Row
+                style={{
+                  backgroundColor: '#00000099',
+                }}
+                h={SUCCESS_RATE_HEIGHT}
+                rounded={15}
+                mb={GAP}
+              >
+                {/* 달성율 */}
+              </L.Row>
+              <L.Row
+                style={{
+                  backgroundColor: '#00000099',
+                }}
+                h={LUCKKIDS_HEIGHT}
+                rounded={15}
+              >
+                {/* 내가 모은 럭키즈 */}
+              </L.Row>
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </View>
       </FrameLayout>
     </>
   );
 };
+
+const SUCCESS_RATE_HEIGHT = 142;
+const GAP = 8;
+const LUCKKIDS_HEIGHT = 157;
+const INITIAL_HEIGHT_REDUCTION = 140;
+
+const CHARACTER_MARGIN = 62;
