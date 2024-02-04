91aff7d0c12449ab9e7d06ff782e1e9d034efe13
gil <longlongroad@naver.com>
2024-02-04 23:03:22 +0900

    [add] 더미 데이터 추가

---
+++ src/data/dummy/data.dummy.garden.ts
@@ -0,0 +1,38 @@
+export const DataDummyGarden = {
+      statusCode: 200,
+      httpStatus: "OK",
+      message: "OK",
+      data: {
+        myProfile: {
+          myId: 1,
+          nickname: "럭키즈",
+          luckPhrases: "행운문구",
+          fileUrl: "assets/images/garden/charactor-lucky.png",
+          characterCount: 1,
+        },
+        friendList: {
+          content: [
+            {
+              friendId: 2,
+              nickname: "럭키즈 친구 2",
+              luckPhrases: "행운 문구 2",
+              fileUrl: "assets/images/garden/charactor-lucky.png",
+              characterCount: 1,
+            },
+            {
+              friendId: 3,
+              nickname: "럭키즈 친구 3",
+              luckPhrases: "행운 문구 3",
+              fileUrl: "assets/images/garden/charactor-lucky.png",
+              characterCount: 2,
+            },
+          ],
+          pageInfo: {
+            currentPage: 1,
+            totalPage: 1,
+            totalElement: 10,
+          },
+        },
+      },
+    }
+;
