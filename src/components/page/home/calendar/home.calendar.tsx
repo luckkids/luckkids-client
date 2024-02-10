87716f27111580c9dcf0e4c63b5216e99b15c591
Gina <koyrkr@gmail.com>
2024-02-10 22:41:16 +0900

    fix: modify calendar style

--- src/components/page/home/calendar/home.calendar.tsx
+++ src/components/page/home/calendar/home.calendar.tsx
@@ -1,74 +1,123 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 import { format } from 'date-fns';
 import { CalendarList, DateData } from 'react-native-calendars';
-import { Colors, Font, L } from '@design-system';
+import { Colors, Font, FontSettings, L } from '@design-system';
 
-const RANGE = 24;
-const initialDate = format(new Date(), 'yyyy-MM-dd');
-const nextWeekDate = '2022-07-14';
-const nextMonthDate = '2022-08-05';
+const initialDate = '2024-02-10'; // 앱 출시 날짜
 
 const HomeCalendar = () => {
-  const [selected, setSelected] = useState(initialDate);
-  const marked = useMemo(() => {
-    return {
-      // [nextWeekDate]: {
-      //   selected: selected === nextWeekDate,
-      //   selectedTextColor: '#5E60CE',
-      //   marked: true,
-      // },
-      // [nextMonthDate]: {
-      //   selected: selected === nextMonthDate,
-      //   selectedTextColor: '#5E60CE',
-      //   marked: true,
-      // },
-      [selected]: {
-        selected: true,
-        disableTouchEvent: true,
-        selectedColor: '#5E60CE',
-        selectedTextColor: 'white',
-      },
-    };
-  }, [selected]);
+  const [selected, setSelected] = useState<string | null>(null);
+  const [activatedDates, setActivatedDates] = useState<string[]>([
+    '2024-02-01',
+    '2024-02-06',
+  ]);
+  const today = format(new Date(), 'yyyy-MM-dd');
+
+  const PAST_RANGE = Math.abs(
+    new Date(initialDate).getMonth() - new Date(today).getMonth(),
+  );
 
   const onDayPress = useCallback((day: DateData) => {
     setSelected(day.dateString);
   }, []);
 
+  const renderDayComponent = useCallback(
+    (date?: DateData) => {
+      if (!date) return <></>;
+      const isSelected = selected === date?.dateString;
+      const isActivated = activatedDates.includes(date?.dateString || '');
+
+      return (
+        <TouchableWithoutFeedback onPress={() => date && onDayPress(date)}>
+          {isSelected ? (
+            <L.Row
+              w={DAY_ITEM_SIZE}
+              h={DAY_ITEM_SIZE}
+              items="center"
+              justify="center"
+              bg={isSelected ? 'LUCK_GREEN' : 'TRANSPARENT'}
+              rounded={DAY_ITEM_SIZE / 2}
+            >
+              <Font type={'BODY_SEMIBOLD'} color={'BLACK'}>
+                {date?.day}
+              </Font>
+            </L.Row>
+          ) : (
+            <L.Row
+              w={DAY_ITEM_SIZE}
+              h={DAY_ITEM_SIZE}
+              items="center"
+              justify="center"
+              rounded={DAY_ITEM_SIZE / 2}
+              style={{
+                backgroundColor: isActivated
+                  ? `${Colors.LUCK_GREEN}40`
+                  : Colors.TRANSPARENT,
+              }}
+            >
+              <Font
+                type={'BODY_SEMIBOLD'}
+                color={isActivated ? 'LUCK_GREEN' : 'GREY1'}
+              >
+                {date?.day}
+              </Font>
+            </L.Row>
+          )}
+        </TouchableWithoutFeedback>
+      );
+    },
+    [selected, activatedDates],
+  );
+
   return (
     <CalendarList
       theme={theme}
       current={initialDate}
-      pastScrollRange={RANGE}
-      futureScrollRange={RANGE}
+      pastScrollRange={PAST_RANGE}
+      futureScrollRange={0}
       onDayPress={onDayPress}
-      markedDates={marked}
       renderHeader={renderCustomHeader}
       horizontal={false}
       pagingEnabled={false}
       staticHeader={false}
+      initialNumToRender={2}
+      keyExtractor={(item) => item.toString()}
       calendarStyle={{
-        backgroundColor: Colors.BLACK,
+        // 캘린더 하나당 스타일
+        backgroundColor: Colors.TRANSPARENT,
       }}
       contentContainerStyle={{
-        backgroundColor: Colors.BLACK,
+        backgroundColor: Colors.TRANSPARENT,
+        paddingBottom: 20,
       }}
       style={{
-        backgroundColor: Colors.BLACK,
+        backgroundColor: Colors.TRANSPARENT,
+        height: '100%',
       }}
+      bounces={false}
+      dayComponent={({ date }) => renderDayComponent(date)}
     />
   );
 };
 
+const DAY_ITEM_SIZE = 45;
+
 const theme = {
-  calendarBackground: Colors.BLACK,
+  calendarBackground: Colors.TRANSPARENT,
+  textDayStyle: {
+    ...FontSettings['BODY_SEMIBOLD'],
+  },
   stylesheet: {
     calendar: {
       header: {
         dayHeader: {
           fontWeight: '600',
-          color: '#48BFE3',
+          color: Colors.RED,
         },
+      },
+      main: {
+        backgroundColor: Colors.TRANSPARENT,
       },
     },
   },
--- src/page/home/page.home.calendar.tsx
+++ src/page/home/page.home.calendar.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import StackNavbar from '@components/common/StackNavBar/StackNavBar';
 import HomeCalendar from '@components/page/home/calendar/home.calendar';
 import { FrameLayout } from '@frame/frame.layout';
+import { L } from '@design-system';
+import { DEFAULT_MARGIN } from '@constants';
+
+const bgImage = require('assets/images/home-calendar-bg.png');
 
 export const PageHomeCalendar: React.FC = () => {
   return (
-    <FrameLayout NavBar={<StackNavbar title="캘린더" />}>
+    <FrameLayout
+      statusBarColor="HOME_CALENDAR_BG"
+      backgroundImage={bgImage}
+      NavBar={<StackNavbar title="캘린더" />}
+    >
       <HomeCalendar />
     </FrameLayout>
   );
