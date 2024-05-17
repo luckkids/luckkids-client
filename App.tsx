91de7664cca107c81d4e9f0e2635dcae58bec885
Gina <koyrkr@gmail.com>
2024-05-17 21:48:11 +0900

    fix: navigate handle when remember me exists

--- App.tsx
+++ App.tsx
@@ -14,13 +14,13 @@ import { ThemeProvider } from 'styled-components/native';
 import { Colors } from '@design-system';
 import { QueryClientProvider } from '@queries';
 import { DataStackScreen } from './src/data/data.stack.screen';
+import { LoginRequest, authApis } from '@apis/auth';
 import withGlobalComponents from '@hooks/hoc/withGlobalComponents';
 import useFirebaseMessage from '@hooks/notification/useFirebaseMessage';
 import useLocalMessage from '@hooks/notification/useLocalMessage';
 import { StorageKeys } from '@hooks/storage/keys';
 import useAsyncStorage from '@hooks/storage/useAsyncStorage';
 import useAsyncEffect from '@hooks/useAsyncEffect';
-import { useFetch } from '@hooks/useFetch';
 import NavigationService from '@libs/NavigationService';
 import { AppScreensParamList, InitialRoute } from '@types-common/page.types';
 
@@ -55,20 +55,28 @@ const RootNavigator = () => {
     StorageKeys.RememberMe,
   );
 
-  const { onFetch: login } = useFetch({
-    method: 'POST',
-    url: '/auth/login',
-    onSuccessCallback: () => {
-      console.log('login success');
-      navigationRef.current?.reset({
-        index: 0,
-        routes: [{ name: 'Home' }],
+  const handleLogin = async (loginInfo: LoginRequest) => {
+    await authApis
+      .login({
+        ...loginInfo,
+      })
+      .then((res) => {
+        const { settingStatus } = res.data;
+        navigationRef.current?.reset({
+          index: 0,
+          routes: [
+            { name: settingStatus === 'COMPLETE' ? 'Home' : 'TutorialStart' },
+          ],
+        });
+      })
+      .catch((error) => {
+        console.error('Login Error', error);
+        setInitialRoute({
+          screenName: 'Login', // rememberMe 정보가 없을 때 로그인 화면으로 이동
+          screenParams: undefined,
+        });
       });
-    },
-    onFailCallback: () => {
-      return;
-    },
-  });
+  };
 
   useEffect(() => {
     initializeFirebaseMessage();
@@ -77,9 +85,8 @@ const RootNavigator = () => {
 
   useEffect(() => {
     // 자동 로그인
-    console.log('rememberMeInfo ====>', rememberMe);
     if (rememberMe) {
-      login({
+      handleLogin({
         email: rememberMe.email,
         password: rememberMe.password,
         deviceId: rememberMe.deviceId,
@@ -87,7 +94,7 @@ const RootNavigator = () => {
       });
     } else {
       setInitialRoute({
-        screenName: 'Login', // rememberMe 정보가 없을 때 로그인 화면으로 이동
+        screenName: 'Login',
         screenParams: undefined,
       });
     }
