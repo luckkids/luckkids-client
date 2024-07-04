ef5ee9f5774f13165b799477f6acadcf40031be8
Gina <koyrkr@gmail.com>
2024-07-04 22:19:41 +0900

    fix: notice item UI

--- src/page/setting/page.setting.notice.tsx
+++ src/page/setting/page.setting.notice.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
-import styled from 'styled-components/native';
+import { format } from 'date-fns';
+import { DEFAULT_MARGIN } from '@constants';
 import { Font, L } from '@design-system';
 import { useSettingNotices } from '@queries';
 import StackNavBar from '@components/common/StackNavBar/StackNavBar';
 import { FrameLayout } from '@frame/frame.layout';
 import useNavigationService from '@hooks/navigation/useNavigationService';
-
-const S = {
-  Text: styled.Text({
-    textAlign: 'center',
-  }),
-  Button: styled.Button({}),
-};
+import { ISettingNotice } from '@types-common/page.types';
 
 export const PageSettingNotice: React.FC = () => {
   const navigation = useNavigationService();
   const { data: notices = [] } = useSettingNotices();
 
+  const handlePressNotice = (notice: ISettingNotice) => {
+    navigation.navigate('WebView', {
+      url: notice.url,
+    });
+  };
+
   return (
     <FrameLayout NavBar={<StackNavBar title={'공지사항'} useBackButton />}>
       {notices?.map((item, i) => {
         return (
-          <TouchableWithoutFeedback onPress={() => console.log('aa')} key={i}>
-            <L.Col pv={25} ph={25}>
+          <TouchableWithoutFeedback
+            onPress={() => handlePressNotice(item)}
+            key={item.id}
+          >
+            <L.Col pv={DEFAULT_MARGIN} ph={DEFAULT_MARGIN} g={8}>
               <Font type={'BODY_REGULAR'}>{item.title}</Font>
               <Font type={'SUBHEADLINE_REGULAR'} color={'GREY1'}>
-                {item.createdDate}
+                {format(item.createdDate, 'yyyy.MM.dd')}
               </Font>
             </L.Col>
           </TouchableWithoutFeedback>
