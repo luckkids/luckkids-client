f235b67be517d680725d2befff757be8af6dd695
gil <longlongroad@naver.com>
2024-07-07 22:07:38 +0900

    [modify] 가든 tsx 코드 분리 및 스낵바 추가

---
+++ src/components/page/garden/garden.bottom.sheet.tsx
@@ -0,0 +1,115 @@
+import { createElement } from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
+import { login } from '@react-native-seoul/kakao-login';
+import styled from 'styled-components/native';
+import { Button, Colors, Font, SvgIcon } from '@design-system';
+import BottomSheet from '@global-components/common/BottomSheet/BottomSheet';
+import SnackBar from '@global-components/common/SnackBar/SnackBar';
+
+const S = {
+  popupWrap: styled.View({
+    backgroundColor: Colors.BG_SECONDARY,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    paddingBottom: 30,
+    paddingHorizontal: 25,
+  }),
+  popupItemContainer: styled.View({
+    marginTop: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }),
+  popupItemWrap: styled.View({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  }),
+  popupItemLogo: styled.View({
+    width: 50,
+    height: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.GREY4,
+    borderRadius: 50,
+  }),
+  buttonWrap: styled.View({
+    marginTop: 35,
+  }),
+};
+
+const onSnackBarHandler = () => {
+  SnackBar.show({
+    leftElement: createElement(SvgIcon, {
+      name: 'lucky_check',
+      size: 20,
+    }),
+    width: 185,
+    title: `링크가 복사됐어요`,
+    position: 'bottom',
+  });
+};
+
+export default function GardenBottomSheet() {
+  const onSendMessage = async () => {
+    try {
+      const result = await login();
+      if (result) {
+        console.log(result);
+        const msg = {
+          templateId: '',
+          templateArgs: {},
+        };
+      } else {
+        console.log('Login Failed');
+      }
+    } catch (error) {
+      console.log('[Error Code]: ', error);
+      /*snackBar.show({
+            })*/
+    }
+  };
+
+  return (
+    <S.popupWrap>
+      <Font type={'HEADLINE_SEMIBOLD'} style={{ marginBottom: 10 }}>
+        친구를 어떻게 부를까요?
+      </Font>
+      <TouchableWithoutFeedback onPress={() => onSendMessage()}>
+        <S.popupItemContainer>
+          <S.popupItemWrap>
+            <S.popupItemLogo style={{ backgroundColor: Colors.KAKAO_YELLOW }}>
+              <SvgIcon name={'iconKakao'} size={20} />
+            </S.popupItemLogo>
+            <Font type={'BODY_REGULAR'} style={{ marginLeft: 16 }}>
+              카카오톡 열기
+            </Font>
+          </S.popupItemWrap>
+          <SvgIcon name={'arrow_right_gray'} size={8} />
+        </S.popupItemContainer>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={() => onSnackBarHandler()}>
+        <S.popupItemContainer>
+          <S.popupItemWrap>
+            <S.popupItemLogo>
+              <SvgIcon name={'iconClip'} size={20} />
+            </S.popupItemLogo>
+            <Font type={'BODY_REGULAR'} style={{ marginLeft: 16 }}>
+              초대 링크 복사
+            </Font>
+          </S.popupItemWrap>
+          <SvgIcon name={'arrow_right_gray'} size={8} />
+        </S.popupItemContainer>
+      </TouchableWithoutFeedback>
+      <S.buttonWrap>
+        <Button
+          type={'action'}
+          text={'닫기'}
+          bgColor={'LUCK_GREEN'}
+          onPress={BottomSheet.hide}
+        />
+      </S.buttonWrap>
+    </S.popupWrap>
+  );
+}
