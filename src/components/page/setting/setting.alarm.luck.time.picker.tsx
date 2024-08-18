949eea969be37b67bbf14d60ab81d39b2ade807b
Gina <koyrkr@gmail.com>
2024-08-18 15:31:38 +0900

    fix: luck time alarm setting picker

--- src/components/page/setting/setting.alarm.luck.time.picker.tsx
+++ src/components/page/setting/setting.alarm.luck.time.picker.tsx
@@ -1,18 +1,10 @@
 import { useState } from 'react';
-import DateTimePicker, {
-  DateTimePickerEvent,
-} from '@react-native-community/datetimepicker';
+import WheelPicker from 'react-native-wheely';
 import styled from 'styled-components/native';
-import { Colors, Font, Button } from '@design-system';
+import { Colors, Font, Button, L, FontSettings } from '@design-system';
 import BottomSheet from '@global-components/common/BottomSheet/BottomSheet';
 
 const S = {
-  disabledButton: styled.View({
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 40,
-  }),
   popupWrap: styled.View({
     backgroundColor: Colors.BG_SECONDARY,
     borderTopLeftRadius: 15,
@@ -20,26 +12,6 @@ const S = {
     paddingBottom: 30,
     paddingTop: 20,
     paddingHorizontal: 25,
-  }),
-  popupItemContainer: styled.View({
-    marginTop: 25,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  }),
-  popupItemWrap: styled.View({
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  }),
-  popupItemLogo: styled.View({
-    width: 50,
-    height: 50,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.GREY4,
-    borderRadius: 50,
   }),
   buttonWrap: styled.View({
     marginTop: 35,
@@ -51,36 +23,90 @@ interface IProps {
   initialTime: number;
 }
 
+const MERIDIEM_OPTIONS = ['오전', '오후'];
+const HOUR_OPTIONS = Array.from({ length: 12 }, (_, i) => i + 1);
+
 export default function SettingAlarmLuckTimePicker({
   onConfirmTime,
   initialTime,
 }: IProps) {
   const [date, setDate] = useState(new Date(initialTime));
 
-  const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
-
-    const tempDate = new Date(currentDate);
-    onConfirmTime(tempDate.getTime());
-  };
-
   return (
     <S.popupWrap>
       <Font type={'HEADLINE_SEMIBOLD'} style={{ marginBottom: 10 }}>
         알림 허용 시간
       </Font>
-      <DateTimePicker
-        testID="dateTimePicker"
-        value={date}
-        is24Hour={false}
-        display="spinner"
-        mode={'time'}
-        onChange={onChange}
-        textColor={Colors.WHITE}
-        locale="ko"
-        minuteInterval={30}
-      />
+      <L.Row w="100%" justify="center" g={30}>
+        {/* meridiem */}
+        <WheelPicker
+          selectedIndex={
+            date.getHours() > 12
+              ? MERIDIEM_OPTIONS.indexOf('오후')
+              : MERIDIEM_OPTIONS.indexOf('오전')
+          }
+          options={MERIDIEM_OPTIONS}
+          onChange={(index) => {
+            const tempDate = new Date(date);
+            const newHour = date.getHours() + (index === 0 ? -12 : 12);
+            tempDate.setHours(newHour);
+            setDate(tempDate);
+          }}
+          itemTextStyle={{
+            ...FontSettings.TITLE2_REGULAR,
+            color: Colors.WHITE,
+          }}
+          containerStyle={{
+            backgroundColor: Colors.TRANSPARENT,
+          }}
+          visibleRest={2}
+          selectedIndicatorStyle={{
+            backgroundColor: Colors.TRANSPARENT,
+          }}
+        />
+        {/* 시간 */}
+        <WheelPicker
+          selectedIndex={HOUR_OPTIONS.indexOf(date.getHours() % 12 || 12)}
+          options={HOUR_OPTIONS.map(String)}
+          onChange={(index) => {
+            const tempDate = new Date(date);
+            tempDate.setHours(
+              HOUR_OPTIONS[index] + (date.getHours() >= 12 ? 12 : 0),
+            );
+            setDate(tempDate);
+          }}
+          visibleRest={2}
+          itemTextStyle={{
+            ...FontSettings.TITLE2_REGULAR,
+            color: Colors.WHITE,
+          }}
+          containerStyle={{
+            backgroundColor: Colors.TRANSPARENT,
+          }}
+          selectedIndicatorStyle={{
+            backgroundColor: Colors.TRANSPARENT,
+          }}
+        />
+        {/* 분 : 00으로 고정 */}
+        <WheelPicker
+          selectedIndex={0}
+          options={['00']}
+          onChange={(index) => {
+            //
+          }}
+          visibleRest={2}
+          itemTextStyle={{
+            ...FontSettings.TITLE2_REGULAR,
+            color: Colors.WHITE,
+          }}
+          containerStyle={{
+            backgroundColor: Colors.TRANSPARENT,
+          }}
+          selectedIndicatorStyle={{
+            backgroundColor: Colors.TRANSPARENT,
+          }}
+        />
+      </L.Row>
       <S.buttonWrap>
         <Button
           type={'action'}
